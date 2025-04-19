2b61cb426fde121e55fb8e7261a9856523bd1ac2
Megz64 <ricgarcia2009@hotmail.com>
2025-04-19 13:53:41 -0600

    Pruebas con servos

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <Arduino.h>
 
-<<<<<<< HEAD
 #include <Servo.h>
 Servo servo1;
 Servo servo2;
@@ -15,83 +14,12 @@ void setup ()
 {
   servo1.attach(3);
   servo2.attach(5);
-  Serial.begin(9600);
-  servo1.write(90);
 
 
-=======
-int motor11=2;
-int motor12=3;
-
-int motor21=4;
-int motor22=5;
-
-int joyY = A0;
-int joyX = A1;
-
-int motor_v1 = 9;
-int motor_v2 = 10;
-
-int full_direction,turn_direction;
-
-//Velocidad 1 y 2 para avanzar con los 2, 3 y 4 para giro a cierta direccion
-int velocidad1,velocidad2,velocidad3,velocidad4;
-
-
-void forward() {
-  analogWrite(motor_v1, velocidad1);
-  analogWrite(motor_v2, velocidad1);
-  digitalWrite(motor12,LOW);
-  digitalWrite(motor22,LOW);
-  digitalWrite(motor11,HIGH);
-  digitalWrite(motor21,HIGH);
-}
-
-void backward() {
-  analogWrite(motor_v1, velocidad2);
-  analogWrite(motor_v2, velocidad2);
-  digitalWrite(motor11,LOW);
-  digitalWrite(motor21,LOW);
-  digitalWrite(motor12,HIGH);
-  digitalWrite(motor22,HIGH);
-}
-
-/*void right() {
-  analogWrite(motor_v1, velocidad3);
-  analogWrite(motor_v2, velocidad3);
-  digitalWrite(motor12,LOW);
-  digitalWrite(motor21,LOW);
-  digitalWrite(motor11,HIGH);
-  digitalWrite(motor22,HIGH);
-}
-
-void left() {
-  analogWrite(motor_v1, velocidad4);
-  analogWrite(motor_v2, velocidad4);
-  digitalWrite(motor11,LOW);
-  digitalWrite(motor22,LOW);
-  digitalWrite(motor12,HIGH);
-  digitalWrite(motor21,HIGH);
-}*/
-
-// put function declarations here:
-
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(motor11,OUTPUT);
-  pinMode(motor12,OUTPUT);
-  pinMode(motor21,OUTPUT);
-  pinMode(motor12,OUTPUT);
-  Serial.begin(4800);
-
-  pinMode(motor_v1,OUTPUT);
-  pinMode(motor_v2,OUTPUT);
->>>>>>> 5fdf71cd521ba6ce1ba412149d170cd1b0fd5c45
 }
 void loop()
 {
 
-<<<<<<< HEAD
   joyValX = analogRead(joyX);
   joyValX = map (joyValX, 0, 1023, 0, 180);
 
@@ -139,44 +67,3 @@ void loop()
     delay(20);
     }
 }
-=======
-void loop() {
-  full_direction = analogRead(joyY);
-  //turn_direction = analogRead(joyX);
-
-  Serial.println(full_direction);
-
-  velocidad1 = map(full_direction, 524, 1023, 0, 225);
-  velocidad2 = map(full_direction, 524, 0, 0, 225);
-  //velocidad3 = map(turn_direction, 524, 0, 0, 225);
-  //velocidad4 = map(turn_direction, 524, 1023, 0, 225);
-
-
-  if (full_direction > 520) {
-    forward();
-    delay(15);
-  } else if (full_direction < 520) {
-    backward();
-    delay(15);
-  } else {
-    digitalWrite(motor11,LOW);
-    digitalWrite(motor21,LOW);
-    digitalWrite(motor12,LOW);
-    digitalWrite(motor22,LOW);
-  }
-
-/*  if (turn_direction > 510 < 530) {
-    right();
-    delay(15);
-  } else if (turn_direction < 510) {
-    left();
-    delay(15);
-  } else {
-    digitalWrite(motor11,LOW);
-    digitalWrite(motor21,LOW);
-    digitalWrite(motor12,LOW);
-    digitalWrite(motor22,LOW);
-  }
-*/
-}
->>>>>>> 5fdf71cd521ba6ce1ba412149d170cd1b0fd5c45
