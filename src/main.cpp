5519b8ae883936318eb9f99b024c43b17dba45e9
Megz64 <ricgarcia2009@hotmail.com>
2025-05-10 11:34:04 -0600

    Pruebas con driver de motores
    Falta el giro

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,111 @@
 #include <Arduino.h>
 
-#include <Servo.h>
-Servo servo1;
-Servo servo2;
-int joyX = A0;
-int joyY = A1;
-int joyValX;
-int joyValY;
+int motor11=2;
+int motor12=3;
 
-void setup ()
-{
-  servo1.attach(5);
-  servo2.attach(3);
-  Serial.begin(9600);
+int motor21=4;
+int motor22=5;
+
+int joyY = A0;
+int joyX = A1;
+
+int motor_v1 = 9;
+int motor_v2 = 10;
+
+int full_direction,turn_direction;
+
+//Velocidad 1 y 2 para avanzar con los 2, 3 y 4 para giro a cierta direccion
+int velocidad1,velocidad2,velocidad3,velocidad4 = 255;
+
+
+void forward() {
+  analogWrite(motor_v1, velocidad1);
+  analogWrite(motor_v2, velocidad1);
+  digitalWrite(motor11, LOW);
+  digitalWrite(motor12, HIGH);
+  digitalWrite(motor21, LOW);
+  digitalWrite(motor22, LOW);
 }
-void loop()
-{
-  joyValX = analogRead(joyX);
-  joyValX = map (joyValX, 0, 1023, 0, 180);
-  servo1.write(joyValX);
-  Serial.write(joyX);
 
-  joyValY = analogRead(joyY);
-  joyValY = map (joyValY, 0, 1023, 0, 180);
-  servo2.write(joyValY);
-  delay(15);  
+void backward() {
+  analogWrite(motor_v1, velocidad2);
+  analogWrite(motor_v2, velocidad2);
+  digitalWrite(motor11, HIGH);
+  digitalWrite(motor12, LOW);
+  digitalWrite(motor21, LOW);
+  digitalWrite(motor22, LOW);
+}
+
+/*void right() {
+  analogWrite(motor_v1, velocidad3);
+  analogWrite(motor_v2, velocidad3);
+  digitalWrite(motor12,LOW);
+  digitalWrite(motor21,LOW);
+  digitalWrite(motor11,HIGH);
+  digitalWrite(motor22,HIGH);
+}
+
+void left() {
+  analogWrite(motor_v1, velocidad4);
+  analogWrite(motor_v2, velocidad4);
+  digitalWrite(motor11,LOW);
+  digitalWrite(motor22,LOW);
+  digitalWrite(motor12,HIGH);
+  digitalWrite(motor21,HIGH);
+}*/
+
+// put function declarations here:
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(motor11,OUTPUT);
+  pinMode(motor12,OUTPUT);
+  pinMode(motor21,OUTPUT);
+  pinMode(motor12,OUTPUT);
+  Serial.begin(4800);
+
+  pinMode(motor_v1,OUTPUT);
+  pinMode(motor_v2,OUTPUT);
+}
+
+void loop() {
+  full_direction = analogRead(joyY);
+  //turn_direction = analogRead(joyX);
+
+  velocidad1 = map(full_direction, 524, 0, 0, 225);
+  velocidad2 = map(full_direction, 524, 1023, 0, 225);
+  //velocidad3 = map(turn_direction, 524, 0, 0, 225);
+  //velocidad4 = map(turn_direction, 524, 1023, 0, 225);
+  Serial.println(velocidad2);
+
+  if (full_direction < 530) {
+    forward();
+    delay(15);
+    
+  } 
+  
+  if (full_direction > 510) {
+    backward();
+    delay(15);
+    
+  }
+
+    /*digitalWrite(motor11,LOW);
+    digitalWrite(motor21,LOW);
+    digitalWrite(motor12,LOW);
+    digitalWrite(motor22,LOW);*/
+
+/*  if (turn_direction > 510 < 530) {
+    right();
+    delay(15);
+  } else if (turn_direction < 510) {
+    left();
+    delay(15);
+  } else {
+    digitalWrite(motor11,LOW);
+    digitalWrite(motor21,LOW);
+    digitalWrite(motor12,LOW);
+    digitalWrite(motor22,LOW);
+  }
+*/
 }
