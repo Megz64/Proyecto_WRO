b7f6ab3c7a8beffc10ca996e2ae30702da744ccc
Megz64 <ricgarcia2009@hotmail.com>
2025-06-10 22:55:03 -0600

    Pruebas de movimiento con steppers, que son muy poco poderosos

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,60 @@
 #include <Arduino.h>
+#include <AccelStepper.h>
 
-// put function declarations here:
-int myFunction(int, int);
+
+
+AccelStepper MotorIzq (AccelStepper::FULL4WIRE, 2, 4, 3, 5);
+AccelStepper MotorDer (AccelStepper::FULL4WIRE, 6, 8, 7, 9);
+
+int joyY = A1;
+int joyX = A0;
+
+
+int full_direction,turn_direction;
+
+
+
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+
+  MotorIzq.setMaxSpeed(1000);
+  MotorDer.setMaxSpeed(1000);
+
+  MotorIzq.setAcceleration(500);
+  MotorDer.setAcceleration(500);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  
+  full_direction = analogRead(joyY);
+  turn_direction = analogRead(joyX);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.println(full_direction);
+
+    MotorIzq.run();
+    MotorDer.run();
+
+  if (full_direction > 530) {
+    MotorIzq.move(-100);
+    MotorDer.move(-100);
+  } 
+  
+  if (full_direction < 510) {
+    MotorIzq.move(100);
+    MotorDer.move(100);
+  }
+
+  if (turn_direction > 530) {
+    MotorIzq.move(-100);
+    MotorDer.move(100);
+  }
+
+  if (turn_direction < 510) {
+    MotorIzq.move(100);
+    MotorDer.move(-100);
+  }
+
 }
